9e394a043f7df66db7eb71f133edbe03b0443363
Thibaut Septon <tsepton@gmail.com>
2023-07-06 16:45:41 +0200

    Wip

--- Runtime/FusionEngine.cs
+++ Runtime/FusionEngine.cs
@@ -10,13 +10,27 @@ namespace Ummi.Runtime {
     public static FusionEngine Instance => Lazy.Value;
     private FusionEngine() { }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="method"></param>
     public void Complete(AttributeParser.RegisteredMMIMethod method) {
       ParameterInfo[] parameters = method.Info.GetParameters();
       foreach (var param in parameters) {
-        var potentialCompletion = FactBase.Instance.GetFacts()
-          .Where(fact => fact.Target.GetType() == param.ParameterType);
-          
+        var facts = GetPotentialCompletionParameters(param);
       }
+    }
+
+    /// <summary>
+    /// Gets the potential facts to complete a method call based on <paramref name="param"/> type.
+    /// </summary>
+    /// <param name="param">The ParameterInfo you need to complete the MethodInfo call</param>
+    /// <returns>An array of Facts ordered by their chronological order of appearance</returns>
+    private Fact[] GetPotentialCompletionParameters(ParameterInfo param) {
+      var potentialCompletion = FactBase.Instance.GetFacts()
+        .Where(fact => fact.Target.GetType() == param.ParameterType)
+        .ToArray();
+      return potentialCompletion;
     }
   }
 }
