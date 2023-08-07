dedcab591013d1d5f54f06d28d3792d244487cc9
Thibaut Septon <tsepton@gmail.com>
2023-08-07 16:22:14 +0200

    Add mouse processor default implementation

--- Runtime/Processors/Mouse.cs
+++ Runtime/Processors/Mouse.cs
@@ -1,7 +1,30 @@
+using System;
+using System.Collections;
+using Ummi.Runtime;
 using ummi.Runtime.Processors;
+using UnityEngine;
 
 namespace ummi.Runtime.Processors {
-  public class Mouse: Processor {
-    
+  /// <summary>
+  /// Process the mouse input modality and interprets it as a raycast.
+  /// 
+  /// When the user clicks using the left button, this processor will add a Ray to the factbase and will also add the
+  /// GameObject (if a Collider is attached) that may have been hit by the ray to the factbase. 
+  /// </summary>
+  public class Mouse : Processor {
+    private void Update() {
+      if (Input.GetMouseButtonDown(0)) OnLeftClick();
+    }
+
+    private Ray MouseToRay() {
+      return Camera.main!.ScreenPointToRay(Input.mousePosition);
+    }
+
+    private void OnLeftClick() {
+      if (Camera.main is null) return;
+      Ray ray = MouseToRay();
+      FactBase.Instance.Add(ray);
+      if (Physics.Raycast(ray, out RaycastHit hit, 250)) FactBase.Instance.Add(hit.collider.gameObject);
+    }
   }
 }
