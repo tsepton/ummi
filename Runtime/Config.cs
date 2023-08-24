44a820d0a581fde3d77e3e5610c1ee8d8a4db214
Thibaut Septon <tsepton@gmail.com>
2023-08-24 11:55:22 +0200

    Implement SBertAPIOrganizer

--- Runtime/Config.cs
+++ Runtime/Config.cs
@@ -1,6 +1,7 @@
 using System;
 using Ummi.Runtime.Speech;
 using Ummi.Runtime.Speech.SBert;
+using Ummi.Runtime.Speech.SBertAPI;
 
 namespace Ummi.Runtime {
   public static class Config {
@@ -9,7 +10,7 @@ namespace Ummi.Runtime {
     public static string DefaultModelPath = "all-MiniLM-L6-v2/model.onnx";
     public static string DefaultVocabularyPath= "all-MiniLM-L6-v2/vocab.txt";
     
-    public static Type ModelOrganizer { get; } = typeof(SBert);
+    public static Type ModelOrganizer { get; } = typeof(SBertAPIOrganizer);
     public static Type SemanticEngine { get; } = typeof(SemanticEngine);
     public static Type FusionEngine { get; } = typeof(MeaningFrameFusionEngine);
   }
--- Runtime/Speech/SBertAPI/SBertAPIOrganizer.cs
+++ Runtime/Speech/SBertAPI/SBertAPIOrganizer.cs
@@ -1,16 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using UnityEngine;
+using UnityEngine.Networking;
+
 namespace Ummi.Runtime.Speech.SBertAPI {
-  public class SBertAPIOrganizer: ModelOrganizer {
-    public SBertAPIOrganizer(ModelPaths paths) : base(paths) { }
+  public class SBertAPIOrganizer : ModelOrganizer {
+    private const string Uri = "http://127.0.0.1:8000/transform/";
+
+    public SBertAPIOrganizer(ModelPaths paths) : base(paths) {
+      // We don't need these as we are using a distant API
+    }
+
     public override IModelOutput Forward(string question) {
       throw new System.NotImplementedException();
     }
 
     public override double[] Predict(string question) {
-      throw new System.NotImplementedException();
+      return GetEmbedding(Uri + question);
     }
 
     public override double[][] Predict(string[] questions) {
-      throw new System.NotImplementedException();
+      double[][] embeddings = new double[questions.Length][];
+      foreach (var (question, i) in questions.Select((str, i) => (str, i))) {
+        embeddings[i] = Predict(question);
+      }
+
+      return embeddings;
+    }
+
+    private double[] GetEmbedding(string utterance) {
+      using (var client = new HttpClient(new HttpClientHandler
+               { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate })) {
+        client.BaseAddress = new Uri(Uri);
+        HttpResponseMessage response = client.GetAsync(utterance).Result;
+        response.EnsureSuccessStatusCode();
+        var x = response.Content.ReadAsStringAsync().Result;
+        string[] strings = x.Replace("[", string.Empty).Replace("]", string.Empty).Split(',');
+        List<double> doubles = new List<double>();
+        foreach (var str in strings) {
+          doubles.Add(Double.Parse(str, System.Globalization.NumberStyles.Float)); // Contains scientific notation
+        }
+
+        return doubles.ToArray();
+      }
     }
   }
 }
